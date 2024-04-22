35c5a27ef59e41458520fb8a2deaec38f0ad4691
Philip Dukshtau <f.dukshtau@gmail.com>
2024-04-22 17:03:38 +0300

    A little bit of reconfiguration

--- src/backend/apps/celery_config.py
+++ src/backend/apps/celery_config.py
@@ -7,7 +7,7 @@ imports = ("apps.tasks",)
 # Using the database to store task state and results.
 result_backend = "redis://redis:6379/0"
 
-task_annotations = {"tasks.add": {"rate_limit": "10/s"}}
+task_annotations = {"*": {"rate_limit": "10/s"}}
 
 task_queues = {
     "ml-queue": {
@@ -18,4 +18,4 @@ task_routes = {"worker.celery_worker.long_task": "ml-queue"}
 task_track_started = True
 
 worker_concurrency = 1
-worker_max_tasks_per_child = 1
+worker_prefetch_multiplier = 1
